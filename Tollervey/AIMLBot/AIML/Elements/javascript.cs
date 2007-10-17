aebbf72395337a7ba69e5c9e4057dae0a03cd6eb
ntoll <ntoll@f517d1a7-a010-0410-ba32-8ed6e74579e7>
2007-10-17 10:38:15 +0000

    made a copy
    git-svn-id: svn://svn.code.sf.net/p/aimlbot/code/trunk@42 f517d1a7-a010-0410-ba32-8ed6e74579e7

---
+++ Tollervey/AIMLBot/AIML/Elements/javascript.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Xml;
+using System.Text;
+
+namespace Tollervey.AIMLBot.AIMLTagHandlers
+{
+    /// <summary>
+    /// NOT IMPLEMENTED FOR SECURITY REASONS
+    /// </summary>
+    public class javascript : AIMLBot.Utils.AIMLTag
+    {
+        /// <summary>
+        /// Ctor
+        /// </summary>
+        /// <param name="bot">The bot involved in this request</param>
+        /// <param name="user">The user making the request</param>
+        /// <param name="query">The query that originated this node</param>
+        /// <param name="request">The request inputted into the system</param>
+        /// <param name="result">The result to be passed to the user</param>
+        /// <param name="templateNode">The node to be processed</param>
+        public javascript(AIMLBot.Bot bot,
+                        AIMLBot.User user,
+                        AIMLBot.Utils.SubQuery query,
+                        AIMLBot.Request request,
+                        AIMLBot.Result result,
+                        XmlNode templateNode)
+            : base(bot, user, query, request, result, templateNode)
+        {
+        }
+
+        protected override string ProcessChange()
+        {
+            this.bot.writeToLog("The javascript tag is not implemented in this bot");
+            return string.Empty;
+        }
+    }
+}
---
+++ Tollervey/AIMLBot/AIML/Elements/uppercase.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Xml;
+using System.Text;
+
+namespace Tollervey.AIMLBot.AIMLTagHandlers
+{
+    /// <summary>
+    /// The uppercase element tells the AIML interpreter to render the contents of the element
+    /// in uppercase, as defined (if defined) by the locale indicated by the specified language
+    /// if specified).
+    /// 
+    /// If no character in this string has a different uppercase version, based on the Unicode 
+    /// standard, then the original string is returned. 
+    /// </summary>
+    public class uppercase : AIMLBot.Utils.AIMLTag
+    {
+        /// <summary>
+        /// Ctor
+        /// </summary>
+        /// <param name="bot">The bot involved in this request</param>
+        /// <param name="user">The user making the request</param>
+        /// <param name="query">The query that originated this node</param>
+        /// <param name="request">The request inputted into the system</param>
+        /// <param name="result">The result to be passed to the user</param>
+        /// <param name="templateNode">The node to be processed</param>
+        public uppercase(AIMLBot.Bot bot,
+                        AIMLBot.User user,
+                        AIMLBot.Utils.SubQuery query,
+                        AIMLBot.Request request,
+                        AIMLBot.Result result,
+                        XmlNode templateNode)
+            : base(bot, user, query, request, result, templateNode)
+        {
+        }
+
+        public override void Render(System.IO.StringWriter writer)
+        {
+            if (this.templateNode.Name.ToLower() == "uppercase")
+            {
+                writer.Write(this.templateNode.InnerText.ToUpper(this.bot.Locale));
+            }
+        }
+    }
+}
