0768d49129e69b1e66bf473a66b795472b452cdb
ntoll <ntoll@f517d1a7-a010-0410-ba32-8ed6e74579e7>
2008-05-07 13:25:49 +0000

    Assembly info update
    git-svn-id: svn://svn.code.sf.net/p/aimlbot/code/trunk@60 f517d1a7-a010-0410-ba32-8ed6e74579e7

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("AimlBot")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A library for building all manner of AIML based conversational agents for the .NET platform")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Nicholas H.Tollervey (ntoll.org)")]
 [assembly: AssemblyProduct("AimlBot")]
-[assembly: AssemblyCopyright("Copyright ©  2008")]
+[assembly: AssemblyCopyright("Copyright © Nicholas H.Tollervey 2008")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
